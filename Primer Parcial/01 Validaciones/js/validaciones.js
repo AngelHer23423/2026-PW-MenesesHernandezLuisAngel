b2b495cd1e7bd474eb766d1af49d93a1ffa5c0d2
AngelHer23423 <lmenesesh2400@alumno.ipn.mx>
2026-09-15 12:32:08 -0600

    Agregando css a validaciones

--- Primer Parcial/01 Validaciones/js/validaciones.js
+++ Primer Parcial/01 Validaciones/js/validaciones.js
@@ -4,9 +4,9 @@
 3. Debe tener un patron para la fecha
 Las expresiones regulares, son patrones que nos ayudan a valdar cadenas bajo ciertas condiciones Mozilla Documentacion*/
 const patrones ={
-    nombre : /^[A-Za-zÁÉÍÓÚáéíóúñÑ\s]{2,60}+$/,
-    boleta : /^\d(10)$/,
-    fecha: /^(0[1-9]|[12]\d|3[01])\/(0[1-9]| 1[0-2])\/d(4)$/
+    nombre : /^[A-Za-zÁÉÍÓÚáéíóúñÑÜü\s]{2,60}$/,
+    boleta : /^\d{10}$/,
+    fecha: /^(0[1-9]|[12]\d|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/
 };
 const mensaje={
     nombre : "Solo letras y espacios, entre 2 y 60 carcteres",
@@ -14,20 +14,20 @@ const mensaje={
     fecha : "La fehca debe tener el formato DD/MM/AAAA."
 } ;
 function validarCampo(campo, valor){
-    returnpatrones[campo].test(valor.trim())
+    return patrones[campo].test(valor.trim())
 }
 //Para validar el formulario tenemos que ocupar los principios de obtenion y manipulacion de los elementos del DOOM
-if(typeof document !== 'undefinided'){
+if(typeof document !== 'undefined'){
     const formulario = document.getElementById("form-registro");
     formulario.addEventListener('submit', (evento)=>{
         evento.preventDefault();
         let formularioValido=true;
         for(const campo of Object.keys(patrones)){
             const input = document.getElementById(campo);
-            const errorSpan= document.getElementById('error-&{campo}');
-            const esValido= validarCampo(campo, input.valuess);
+            const errorSpan= document.getElementById(`error-${campo}`);
+            const esValido= validarCampo(campo, input.value);
             input.classList.toggle('invalido', !esValido)
-            spanError.textContent= esValido ? '' : mensaje[campo];
+            errorSpan.textContent= esValido ? '' : mensaje[campo];
             if(!esValido) formularioValido=false;
         }
         const mensajeExito =document.getElementById('mensaje-exito');
