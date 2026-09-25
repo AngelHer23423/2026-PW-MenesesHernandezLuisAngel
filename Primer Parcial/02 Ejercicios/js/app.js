82f56a86396aae6535dd7fb8b8a58eeac28a9b10
AngelHer23423 <lmenesesh2400@alumno.ipn.mx>
2026-09-24 18:20:56 -0600

    Areglando app.js

--- Primer Parcial/02 Ejercicios/js/app.js
+++ Primer Parcial/02 Ejercicios/js/app.js
@@ -79,11 +79,21 @@ formObjeto.addEventListener('submit', (evento) => {
         case 'values':
             break;
         case 'entries':
-            resultado= Object.entries(taller).map(([campo,valor]) => `${campo}:${valor}`).join('\n');
+            resultado = Object.entries(taller).map(([campo,valor]) => `${campo}: ${valor}`).join('\n');
             break;
         case 'stringify':
             break;
         case 'roundtrip':
+            const textoJson = JSON.stringify(taller, null, 2);
+            const objetoDeVuelta = JSON.parse(textoJson);
+
+            resultado = [
+                '',
+                textoJson,
+                '',
+                `tipo: ${typeof objetoDeVuelta}`,
+                objetoDeVuelta.nombre
+            ].join('\n');
             break;
     }
 })
